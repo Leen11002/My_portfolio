7b259086e2a5674a541dcfdc14a5f7c263fa9301
leen <leenaboalhijaa@gmail.com>
2025-03-20 00:10:11 +0300

    first

---
+++ my-portfolio/src/components/ContactMeForm.jsx
@@ -0,0 +1,125 @@
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import check from '../assets/check.png'
+import cross from '../assets/cross.png'
+export default function ContactMeForm() {
+  
+  const formInitialDetails = {
+    fullName: "",
+    email: "",
+    message: "",
+  };
+  const [formDetails, setFormDetails] = useState(formInitialDetails);
+  const [buttonText, setButtonText] = useState("Send");
+  const [status, setStatus] = useState({});
+  const onFormUpdate=(category , value)=>{
+    setFormDetails({
+        ...formDetails,
+        [category]:value
+    })
+  }
+  const handleSubmit=async(e)=>{
+    e.preventDefault();
+    console.log(formDetails);
+    setButtonText('Sending...');
+    let response= await fetch('https://my-portfolio-server-yi9y.vercel.app/contact',{
+        method:'POST',
+        headers:{
+            "Content-Type":"Application/json;charset=utf-8",
+        },
+        body:JSON.stringify(formDetails),
+    });
+    setButtonText('Send');
+    let result=await response.json();
+    setFormDetails(formInitialDetails);
+    if(result.code===200){
+         setStatus({success:true , message:'Message sent successfully',})
+    } else{
+        setStatus({success:false , message:'Somthing went wrong, please try agail later.'})
+    }
+  }
+  return (
+    <div
+      className="grid col p-3 rounded-3"
+    >
+      <p className="text-start fw-bold" style={{ fontFamily: '"Rubik", serif' }}>
+        Feel free to reach out to me anytime!
+      </p>
+      <form onSubmit={handleSubmit} className="d-flex flex-column gap-1 text-start ">
+        <input
+          type="text"
+          placeholder="Full Name"
+          className="rounded-2 p-2 mb-2"
+          style={{
+            backgroundColor: "#FAF3EC",
+            border: "none",
+            color: "#393939",
+            maxWidth: "300px",
+            fontFamily: '"Roboto" , sans-serif',
+          }}
+          required
+          value={formDetails.fullName}
+          onChange={(e)=>{onFormUpdate('fullName',e.target.value)}}
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          className="rounded-2 p-2 mb-2"
+          style={{
+            backgroundColor: "#FAF3EC",
+            border: "none",
+            color: "#393939",
+            maxWidth: "300px",
+            fontFamily: '"Roboto" , sans-serif',
+          }}
+          required
+          value={formDetails.email}
+          onChange={(e)=>{onFormUpdate('email',e.target.value)}}
+        />
+        <textarea
+         
+          placeholder="Your Message"
+          className="rounded-2 p-3"
+          style={{
+            backgroundColor: "#FAF3EC",
+            border: "none",
+            color: "#393939",
+            maxWidth: "500px",
+            height: "250px",
+            fontFamily: '"Roboto" , sans-serif',
+            resize: "none",
+            fontSize: "12px",
+          }}
+          required
+          value={formDetails.message}
+          onChange={(e)=>{onFormUpdate('message',e.target.value)}}
+        ></textarea>
+        {status.message &&
+        <p className="text-start" style={{fontFamily:'"Montserrat", serif', color:status.color}}>{status.message}
+        {status
+        .success ?
+        <img className="ms-1" src={check}/>: <img className="ms-1"  src={cross}/>}</p>
+        }
+        <div
+          className="d-flex flex-row justify-content-end align-items-start pt-0 mt-0"
+          style={{ width: "100%" }}
+        >
+          <Button
+            className="mt-4 pt-2 pb-2 ps-4 pe-4 rounded-4"
+            type="submit"
+            style={{
+              boxShadow: "0px 4px 4px rgb(0,0,0,25%)",
+              border: "none",
+              fontWeight: "500",
+              backgroundColor: "#60A7F9",
+              fontFamily:'"Rubik" , serif'
+            }}
+          >
+            {buttonText}
+          </Button>
+        </div>
+
+      </form>
+    </div>
+  );
+}
---
+++ my-portfolio/src/components/MainInformation.jsx
@@ -0,0 +1,79 @@
+import my_photo from "../assets/my_photo.png";
+import Button from "react-bootstrap/Button";
+import AOS from "aos";
+import "aos/dist/aos.css"; // Import AOS styles
+import { useEffect } from "react";
+export default function MainInformation() {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000, // Animation duration (in milliseconds)
+      offset: 100, // Offset (in pixels) from the original trigger point
+    });
+  }, []);
+
+  return (
+    <main
+      id="home"
+      className="container row gap-5 mb-5 p-5 conrtollGap"
+      data-aos="fade-down"
+    >
+      <section className="col d-flex-col ms-0 align-items-center justify-content-end text-start mt-5 pt-5   responsiveWidth">
+        <p className="h6" style={{ fontFamily: '"Roboto", sans-serif' }}>
+          Hi, my name is leen.
+        </p>
+        <p
+          className="h1 bigText"
+          style={{
+            
+            fontFamily: '"Kadwa", serif',
+            fontWeight: "bold",
+          }}
+        >
+          I Craft logic for
+        </p>
+        <p
+          className="h1 bigText"
+          style={{
+            marginTop: "-15px",
+            fontFamily: '"Kadwa", serif',
+            fontWeight: "bold",
+          }}
+        >
+          growth
+        </p>
+        <p className="h6 mt-3" style={{ fontFamily: '"Roboto", sans-serif' }}>
+          Junior Front-End Web Developer with three-months internship experience
+          in web development
+        </p>
+        <a
+          href="#Contact"
+          className="text-decoration-none"
+          style={{ color: "black" }}
+        >
+          {" "}
+          <Button
+            className="mt-4 ps-4 pe-4 pt-2 pb-2 rounded-4"
+            style={{
+              boxShadow: "0px 4px 4px rgb(0,0,0,25%)",
+              fontWeight: "500",
+              backgroundColor: "#F8CF4B",
+              fontFamily:'"Rubik" , serif',
+              border: "none",
+
+            }}
+          >
+            Contact Me
+          </Button>
+        </a>
+      </section>
+      <section className="col d-flex align-items-center justify-content-center">
+        <img
+          src={my_photo}
+          alt="photo of me"
+          className="p-3 smallScreenWidthAndHieght"
+          style={{ marginTop: "80px" }}
+        />
+      </section>
+    </main>
+  );
+}
